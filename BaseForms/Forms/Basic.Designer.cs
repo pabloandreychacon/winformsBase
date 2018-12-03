c0be285c94afe8d07ab4dd0e2fe5371d35264dd3
Andrey Chacon <ext-achacon@prevalent.net>
2018-12-03 16:43:55 -0600

    sizable base

--- BaseForms/Forms/Basic.Designer.cs
+++ BaseForms/Forms/Basic.Designer.cs
@@ -70,6 +70,7 @@
 			this.Controls.Add(this.lblInfoMessage);
 			this.Controls.Add(this.errorContainer1);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
 			this.MaximizeBox = false;
