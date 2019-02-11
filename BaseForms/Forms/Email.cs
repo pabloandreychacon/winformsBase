fd0bc5f476418bf32840aa915b6c67315566f6e1
Pablo Andrey Chacon Luna <pabloandreychacon@hotmail.com>
2019-02-10 19:22:50 -0600

    tools email

--- BaseForms/Forms/Email.cs
+++ BaseForms/Forms/Email.cs
@@ -20,8 +20,8 @@ namespace BaseForms.Forms
         private string password;
         private string from;
         private string displayName;
-        public Email(int pPort = 587, int pTimeOut = 60000, string pHost = "smtp.gmail.com", 
-            string pUserName = "pabloandreychacon@gmail.com", string pPassword = "", 
+        public Email(int pPort = 587, int pTimeOut = 60000, string pHost = "smtp.gmail.com",
+            string pUserName = "pabloandreychacon@gmail.com", string pPassword = "",
             string pFrom = "pabloandreychacon@gmail.com", string pDisplayName = "Pablo Andrey")
         {
             InitializeComponent();
@@ -57,55 +57,30 @@ namespace BaseForms.Forms
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Control", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-            }            
+            }
             this.lblEnviando.Visible = false;
         }
 
         public void SendEmail(string sPara, string sAsunto, string sMensaje, bool isHtml = false)
         {
-            SmtpClient smtp = new SmtpClient();
-            smtp.Port = this.port;// 25;//El puerto debe ser correctamente configurado. (puertos: 25,587, o 465)
-            smtp.Timeout = this.timeout; //60000;//Tiempo de conexión en milisegundos
-            smtp.Host = this.host;// ;
-            smtp.EnableSsl = true;
-            smtp.Credentials = new System.Net.NetworkCredential
-                { UserName = this.userName, Password = this.password };
-            //Objeto referente a quién envía el correo
-            MailAddress from = new MailAddress(this.from, this.displayName, System.Text.Encoding.UTF8);
-            var mails = sPara.Split(';');
-            using (MailMessage message = new MailMessage())
+
+            try
             {
-                message.From = from;
-                message.Subject = sAsunto;
-                //Cargamos el contenido del mail.
-                message.Body = sMensaje;
-                message.Priority = MailPriority.High;
-                message.IsBodyHtml = isHtml;
-                if (this.isAttachment == true)
-                {
-                    Attachment adjunto = new Attachment(this.txtArchivo.Text);
-                    message.Attachments.Add(adjunto);
-                    this.isAttachment = false;
-                    this.txtArchivo.Clear();
-                }
-                //Cargamos todos los mails que se ingresaron
-                //en el campor de text
-                foreach (string dir in mails)
-                    message.To.Add(dir);
-                try
-                {
-                    //Se envía el mail
-                    smtp.Send(message);
-                    MessageBox.Show("El email fue enviado");
-                }
-                catch (SmtpException ex)
-                {
-                    //throw;
-                    //MessageBox.Show("error:" + ex.Message);
-                    MessageBox.Show(ex.Message, "Control", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-                }
+                //Se envía el mail
+                BaseHelpers.Helpers.EmailTools.SendEmail(sMensaje, this.port, this.timeout, this.host,
+                this.userName, this.password, sAsunto,
+                this.from, this.displayName, sPara, isHtml);
+                MessageBox.Show("El email fue enviado");
             }
+            catch (SmtpException ex)
+            {
+                //throw;
+                //MessageBox.Show("error:" + ex.Message);
+                MessageBox.Show(ex.Message, "Control", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+
         }
+
         private void adjuntarBtn_Click(object sender, EventArgs e)
         {
             AdjuntarArchivo();
@@ -127,7 +102,7 @@ namespace BaseForms.Forms
             }
             catch (Exception ex)
             {
-                MessageBox.Show("No se pudo adjuntar el archivo! " + ex.Message,"Email adjunto", 
+                MessageBox.Show("No se pudo adjuntar el archivo! " + ex.Message, "Email adjunto",
                     MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
             }
         }
---
+++ BaseHelpers/Helpers/EmailTools.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseHelpers.Helpers
+{
+    public class EmailTools
+    {
+        public static void SendEmail(string mensajeBody, int port, int timeout, string host, string userName, string password,
+            string asunto, string fromEmail, string displayName, string destinos, bool isHtml = false)
+        {
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                SmtpClient smtp = new SmtpClient
+                {
+                    Port = port,// 25;//El puerto debe ser correctamente configurado. (puertos: 25,587, o 465)
+                    Timeout = timeout, //60000;//Tiempo de conexión en milisegundos
+                    Host = host,// ;
+                    EnableSsl = true,
+                    Credentials = new System.Net.NetworkCredential
+                    { UserName = userName, Password = password }
+                };
+                //Objeto referente a quién envía el correo
+                MailAddress from = new MailAddress(fromEmail, displayName, System.Text.Encoding.UTF8);
+                var mails = destinos.Split(';');
+                using (MailMessage message = new MailMessage())
+                {
+                    message.From = from;
+                    message.Subject = asunto;
+                    //Cargamos el contenido del mail.
+                    message.Body = mensajeBody;
+                    message.Priority = MailPriority.High;
+                    message.IsBodyHtml = isHtml;
+
+                    //Cargamos todos los mails que se ingresaron
+                    //en el campor de text
+                    foreach (string dir in mails)
+                        message.To.Add(dir);
+                    try
+                    {
+                        //Se envía el mail
+                        smtp.Send(message);
+                        //MessageBox.Show("El email fue enviado");
+                    }
+                    catch 
+                    {
+                        throw;
+                    }
+                }
+            }
+            else
+            {
+                var myType = typeof(EmailTools);
+                var ns = myType.Namespace;
+                throw new Exception("No hay conexión a Internet, imposible enviar correo. " + ns);
+            }
+        }
+    }
+}
